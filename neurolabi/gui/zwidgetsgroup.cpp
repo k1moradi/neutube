448f85c3c4f2a7bb2ec730fa946491d7a7cb5d12
k1moradi <k.moradi@gmail.com>
2025-10-30 18:43:56 -0700

    code reformatting + Rewrite setVisible() to recursively propagate visibility to all child parameters/widgets.

--- neurolabi/gui/zwidgetsgroup.cpp
+++ neurolabi/gui/zwidgetsgroup.cpp
@@ -1,12 +1,11 @@
 #include "zwidgetsgroup.h"
+#include <QtGui>
 #include "z3dcameraparameter.h"
 #include "zparameter.h"
-#include <QtGui>
 #ifdef _QT5_
 #include <QtWidgets>
 #endif
-
-ZWidgetsGroup::ZWidgetsGroup(QWidget *widget, ZWidgetsGroup *parentGroup, int visibleLevel, QObject *parent)
+ZWidgetsGroup::ZWidgetsGroup(QWidget* widget, ZWidgetsGroup* parentGroup, int visibleLevel, QObject* parent)
   : QObject(parent)
   , m_type(WIDGET)
   , m_groupName("This is not a group")
@@ -15,13 +14,11 @@ ZWidgetsGroup::ZWidgetsGroup(QWidget *widget, ZWidgetsGroup *parentGroup, int vi
   , m_parent(parentGroup)
   , m_visibleLevel(visibleLevel)
   , m_isSorted(true)
-  , m_isVisible(true)
-{
-  if (parentGroup)
+  , m_isVisible(true) {
+  if(parentGroup)
     parentGroup->addChildGroup(this);
 }
-
-ZWidgetsGroup::ZWidgetsGroup(const QString &groupName, ZWidgetsGroup *parentGroup, int visibleLevel, QObject *parent)
+ZWidgetsGroup::ZWidgetsGroup(const QString& groupName, ZWidgetsGroup* parentGroup, int visibleLevel, QObject* parent)
   : QObject(parent)
   , m_type(GROUP)
   , m_groupName(groupName)
@@ -31,13 +28,11 @@ ZWidgetsGroup::ZWidgetsGroup(const QString &groupName, ZWidgetsGroup *parentGrou
   , m_visibleLevel(visibleLevel)
   , m_isSorted(false)
   , m_cutOffbetweenBasicAndAdvancedLevel(1)
-  , m_isVisible(true)
-{
-  if (parentGroup)
+  , m_isVisible(true) {
+  if(parentGroup)
     parentGroup->addChildGroup(this);
 }
-
-ZWidgetsGroup::ZWidgetsGroup(ZParameter *parameter, ZWidgetsGroup *parentGroup, int visibleLevel, QObject *parent)
+ZWidgetsGroup::ZWidgetsGroup(ZParameter* parameter, ZWidgetsGroup* parentGroup, int visibleLevel, QObject* parent)
   : QObject(parent)
   , m_type(PARAMETER)
   , m_groupName("This is not a group")
@@ -46,107 +41,103 @@ ZWidgetsGroup::ZWidgetsGroup(ZParameter *parameter, ZWidgetsGroup *parentGroup, 
   , m_parent(parentGroup)
   , m_visibleLevel(visibleLevel)
   , m_isSorted(true)
-  , m_isVisible(true)
-{
-  if (parentGroup)
+  , m_isVisible(true) {
+  if(parentGroup)
     parentGroup->addChildGroup(this);
 }
-
-ZWidgetsGroup::~ZWidgetsGroup()
-{
-  if (m_parent)
+ZWidgetsGroup::~ZWidgetsGroup() {
+  if(m_parent)
     m_parent->removeChildGroup(this);
-  if (m_type == GROUP)
-    for (int i=0; i<m_childGroups.size(); i++) {
+  if(m_type == GROUP)
+    for(int i = 0; i < m_childGroups.size(); i++) {
       delete m_childGroups[i];
     }
 }
-
-void ZWidgetsGroup::setVisible(bool visible)
-{
+void ZWidgetsGroup::setVisible(bool visible) {
+  if(m_isVisible == visible) return;
   m_isVisible = visible;
+  // Propagate to children based on type
+  switch(m_type) {
+  case PARAMETER:
+    if(m_parameter) m_parameter->setVisible(visible);
+    break;
+  case WIDGET:
+    if(m_widget) m_widget->setVisible(visible);
+    break;
+  case GROUP:
+    for(int i = 0; i < m_childGroups.size(); i++) {
+      m_childGroups[i]->setVisible(visible);
+    }
+    break;
+  }
 }
-
-const QList<ZWidgetsGroup *> &ZWidgetsGroup::getChildGroups()
-{
-  if (!m_isSorted)
+const QList<ZWidgetsGroup*>& ZWidgetsGroup::getChildGroups() {
+  if(!m_isSorted)
     sortChildGroups();
   return m_childGroups;
 }
-
-void ZWidgetsGroup::addChildGroup(ZWidgetsGroup *child, bool atEnd)
-{
-  if (child->m_parent && child->m_parent != this)
+void ZWidgetsGroup::addChildGroup(ZWidgetsGroup* child, bool atEnd) {
+  if(child->m_parent && child->m_parent != this)
     child->m_parent->removeChildGroup(child);
   child->m_parent = this;
-  if (atEnd)
+  if(atEnd)
     m_childGroups.push_back(child);
   else
     m_childGroups.push_front(child);
   connect(child, SIGNAL(widgetsGroupChanged()), this, SLOT(emitWidgetsGroupChangedSignal()));
   m_isSorted = false;
 }
-
-void ZWidgetsGroup::removeChildGroup(ZWidgetsGroup *child)
-{
+void ZWidgetsGroup::removeChildGroup(ZWidgetsGroup* child) {
   int num = m_childGroups.removeAll(child);
   child->m_parent = NULL;
-  if (num > 0)
+  if(num > 0)
     child->disconnect(this);
 }
-
-void ZWidgetsGroup::mergeGroup(ZWidgetsGroup *other, bool atEnd)
-{
-  if (other->m_type != GROUP) {
-      addChildGroup(other, atEnd);
+void ZWidgetsGroup::mergeGroup(ZWidgetsGroup* other, bool atEnd) {
+  if(other->m_type != GROUP) {
+    addChildGroup(other, atEnd);
   } else {
-    if (atEnd) {
-      while (other->m_childGroups.size() > 0) {
+    if(atEnd) {
+      while(other->m_childGroups.size() > 0) {
         addChildGroup(other->m_childGroups[0], atEnd);
       }
     } else {
-      for (int i=other->m_childGroups.size()-1; i>=0; i--) {
+      for(int i = other->m_childGroups.size() - 1; i >= 0; i--) {
         addChildGroup(other->m_childGroups[i], atEnd);
       }
     }
   }
 }
-
-QWidget *ZWidgetsGroup::createWidget(QMainWindow *mainWin, bool createBasic)
-{
-  QLayout *lw = createLayout(mainWin, createBasic);
+QWidget* ZWidgetsGroup::createWidget(QMainWindow* mainWin, bool createBasic) {
+  QLayout* lw = createLayout(mainWin, createBasic);
   // if is boxLayout, add strech to fill the space
-  QBoxLayout *blo = qobject_cast<QBoxLayout*>(lw);
-  if (blo)
+  QBoxLayout* blo = qobject_cast<QBoxLayout*>(lw);
+  if(blo)
     blo->addStretch();
-  QWidget *widget = new QWidget();
+  QWidget* widget = new QWidget();
   widget->setLayout(lw);
-  QScrollArea * sa = new QScrollArea();
+  QScrollArea* sa = new QScrollArea();
   sa->setWidgetResizable(true);
   sa->setWidget(widget);
-
-//  sa->setVisible(isVisible());
-
+  //  sa->setVisible(isVisible());
   return sa;
 }
-
-QLayout *ZWidgetsGroup::createLayout(QMainWindow *mainWin, bool createBasic)
-{
-  switch (m_type) {
+QLayout* ZWidgetsGroup::createLayout(QMainWindow* mainWin, bool createBasic) {
+  switch(m_type) {
   case WIDGET: {
-    QHBoxLayout *hbl = new QHBoxLayout;
+    QHBoxLayout* hbl = new QHBoxLayout;
     hbl->addWidget(m_widget);
     return hbl;
   }
   case PARAMETER: {
-    QHBoxLayout *hbl = new QHBoxLayout;
-    if (qobject_cast<Z3DCameraParameter*>(m_parameter)) {
+    QHBoxLayout* hbl = new QHBoxLayout;
+    if(qobject_cast<Z3DCameraParameter*>(m_parameter)) {
       QWidget* wg = m_parameter->createWidget();
       hbl->addWidget(wg);
       return hbl;
     }
     m_parameter->setMainWindow(mainWin);
-    QLabel *label = m_parameter->createNameLabel();
+    QLabel* label = m_parameter->createNameLabel();
     label->setMinimumWidth(125);
     label->setWordWrap(true);
     hbl->addWidget(label);
@@ -154,36 +145,34 @@ QLayout *ZWidgetsGroup::createLayout(QMainWindow *mainWin, bool createBasic)
     wg->setMinimumWidth(175);
     wg->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
     hbl->addWidget(wg);
-
     return hbl;
   }
   default: /*case GROUP:*/ {
-    QVBoxLayout *vbl = new QVBoxLayout;
-    if (!m_isSorted)
+    QVBoxLayout* vbl = new QVBoxLayout;
+    if(!m_isSorted)
       sortChildGroups();
-    if (createBasic) {
+    if(createBasic) {
       int i;
-      for (i=0; i<m_childGroups.size() && m_childGroups[i]->m_visibleLevel
-           <= m_cutOffbetweenBasicAndAdvancedLevel; i++) {
-        QLayout *lw = m_childGroups[i]->createLayout(mainWin, true);
-        if (m_childGroups[i]->isGroup()) {
-          QGroupBox *groupBox = new QGroupBox(m_childGroups[i]->getGroupName());
+      for(i = 0; i < m_childGroups.size() && m_childGroups[i]->m_visibleLevel <= m_cutOffbetweenBasicAndAdvancedLevel; i++) {
+        QLayout* lw = m_childGroups[i]->createLayout(mainWin, true);
+        if(m_childGroups[i]->isGroup()) {
+          QGroupBox* groupBox = new QGroupBox(m_childGroups[i]->getGroupName());
           groupBox->setLayout(lw);
           vbl->addWidget(groupBox);
         } else
           vbl->addLayout(lw);
       }
-      if (i<m_childGroups.size()) {
-        QPushButton *pb = new QPushButton("Advanced...");
+      if(i < m_childGroups.size()) {
+        QPushButton* pb = new QPushButton("Advanced...");
         pb->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
         vbl->addWidget(pb, 0, Qt::AlignHCenter | Qt::AlignVCenter);
         connect(pb, SIGNAL(clicked()), this, SLOT(emitRequestAdvancedWidgetSignal()));
       }
     } else {
-      for (int i=0; i<m_childGroups.size(); i++) {
-        QLayout *lw = m_childGroups[i]->createLayout(mainWin, false);
-        if (m_childGroups[i]->isGroup()) {
-          QGroupBox *groupBox = new QGroupBox(m_childGroups[i]->getGroupName());
+      for(int i = 0; i < m_childGroups.size(); i++) {
+        QLayout* lw = m_childGroups[i]->createLayout(mainWin, false);
+        if(m_childGroups[i]->isGroup()) {
+          QGroupBox* groupBox = new QGroupBox(m_childGroups[i]->getGroupName());
           groupBox->setLayout(lw);
           vbl->addWidget(groupBox);
         } else
@@ -194,29 +183,19 @@ QLayout *ZWidgetsGroup::createLayout(QMainWindow *mainWin, bool createBasic)
   }
   }
 }
-
-bool ZWidgetsGroup::operator <(const ZWidgetsGroup &other) const
-{
+bool ZWidgetsGroup::operator<(const ZWidgetsGroup& other) const {
   return m_visibleLevel < other.m_visibleLevel;
 }
-
-bool __widgetGroupPtVisibleLevelLessThan(const ZWidgetsGroup *s1, const ZWidgetsGroup *s2)
- {
-     return s1->getVisibleLevel() < s2->getVisibleLevel();
- }
-
-void ZWidgetsGroup::sortChildGroups()
-{
+bool __widgetGroupPtVisibleLevelLessThan(const ZWidgetsGroup* s1, const ZWidgetsGroup* s2) {
+  return s1->getVisibleLevel() < s2->getVisibleLevel();
+}
+void ZWidgetsGroup::sortChildGroups() {
   qStableSort(m_childGroups.begin(), m_childGroups.end(), __widgetGroupPtVisibleLevelLessThan);
   m_isSorted = true;
 }
-
-void ZWidgetsGroup::emitRequestAdvancedWidgetSignal()
-{
+void ZWidgetsGroup::emitRequestAdvancedWidgetSignal() {
   emit requestAdvancedWidget(m_groupName);
 }
-
-void ZWidgetsGroup::emitWidgetsGroupChangedSignal()
-{
+void ZWidgetsGroup::emitWidgetsGroupChangedSignal() {
   emit widgetsGroupChanged();
 }
--- neurolabi/gui/zwidgetsgroup.h
+++ neurolabi/gui/zwidgetsgroup.h
@@ -1,65 +1,52 @@
 #ifndef ZWIDGETSGROUP_H
 #define ZWIDGETSGROUP_H
-
-#include <QObject>
 #include <QLayout>
-
+#include <QObject>
 class ZParameter;
 class QMainWindow;
-
-class ZWidgetsGroup : public QObject
-{
+class ZWidgetsGroup : public QObject {
   Q_OBJECT
 public:
   enum type {
-    GROUP, WIDGET, PARAMETER
+    GROUP,
+    WIDGET,
+    PARAMETER
   };
-
   explicit ZWidgetsGroup(QWidget* widget, ZWidgetsGroup* parentGroup,
-                         int visibleLevel, QObject *parent = 0);
-  explicit ZWidgetsGroup(const QString &groupName, ZWidgetsGroup* parentGroup,
-                         int visibleLevel, QObject *parent = 0);
-  explicit ZWidgetsGroup(ZParameter *parameter, ZWidgetsGroup* parentGroup,
-                         int visibleLevel, QObject *parent = 0);
+    int visibleLevel, QObject* parent = 0);
+  explicit ZWidgetsGroup(const QString& groupName, ZWidgetsGroup* parentGroup,
+    int visibleLevel, QObject* parent = 0);
+  explicit ZWidgetsGroup(ZParameter* parameter, ZWidgetsGroup* parentGroup,
+    int visibleLevel, QObject* parent = 0);
   virtual ~ZWidgetsGroup();
-
-  inline bool isGroup() const {return m_type == GROUP;}
-  inline QString getGroupName() const {return m_groupName;}
+  inline bool isGroup() const { return m_type == GROUP; }
+  inline QString getGroupName() const { return m_groupName; }
   const QList<ZWidgetsGroup*>& getChildGroups();
-
-  void addChildGroup(ZWidgetsGroup *child, bool atEnd = true);
+  void addChildGroup(ZWidgetsGroup* child, bool atEnd = true);
   void removeChildGroup(ZWidgetsGroup* child);
-
   void mergeGroup(ZWidgetsGroup* other, bool atEnd = true);
-
-  inline int getVisibleLevel() const {return m_visibleLevel;}
-  inline void setVisibleLevel(int v) {m_visibleLevel=v;}
-  inline void setBasicAdvancedCutoff(int v) {m_cutOffbetweenBasicAndAdvancedLevel=v;}
-  inline int getBasicAdvancedCutoff() const {return m_cutOffbetweenBasicAndAdvancedLevel;}
+  inline int getVisibleLevel() const { return m_visibleLevel; }
+  inline void setVisibleLevel(int v) { m_visibleLevel = v; }
+  inline void setBasicAdvancedCutoff(int v) { m_cutOffbetweenBasicAndAdvancedLevel = v; }
+  inline int getBasicAdvancedCutoff() const { return m_cutOffbetweenBasicAndAdvancedLevel; }
   void setVisible(bool visible);
   inline bool isVisible() { return m_isVisible; }
-
-  QWidget *createWidget(QMainWindow *mainWin, bool createBasic = true);
-  QLayout *createLayout(QMainWindow *mainWin, bool createBasic = true);
-
+  QWidget* createWidget(QMainWindow* mainWin, bool createBasic = true);
+  QLayout* createLayout(QMainWindow* mainWin, bool createBasic = true);
   bool operator<(const ZWidgetsGroup& other) const;
-  
 signals:
-  void requestAdvancedWidget(const QString &name);
+  void requestAdvancedWidget(const QString& name);
   void widgetsGroupChanged();
-  
 public slots:
   void emitRequestAdvancedWidgetSignal();
   void emitWidgetsGroupChangedSignal();
-
 private:
   void sortChildGroups();
-  
 private:
   type m_type;
   QString m_groupName;
-  QWidget *m_widget;
-  ZParameter *m_parameter;
+  QWidget* m_widget;
+  ZParameter* m_parameter;
   ZWidgetsGroup* m_parent;
   int m_visibleLevel;
   bool m_isSorted;
@@ -67,5 +54,4 @@ private:
   QList<ZWidgetsGroup*> m_childGroups;
   bool m_isVisible;
 };
-
 #endif // ZWIDGETSGROUP_H
