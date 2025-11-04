09142f9756732321b6aae844519bfbd97bc32ca4
k1moradi <k.moradi@gmail.com>
2025-11-03 18:41:14 -0800

    code reformatting

--- neurolabi/gui/dialogs/autosaveswclistdialog.cpp
+++ neurolabi/gui/dialogs/autosaveswclistdialog.cpp
@@ -1,76 +1,62 @@
 #include "autosaveswclistdialog.h"
-
 #include <iostream>
-#include "ui_autosaveswclistdialog.h"
 #include "neutubeconfig.h"
-#include "zstackframe.h"
 #include "tz_error.h"
+#include "ui_autosaveswclistdialog.h"
+#include "zstackframe.h"
 #include "zwindowfactory.h"
+#include <algorithm>
 
-AutosaveSwcListDialog::AutosaveSwcListDialog(QWidget *parent) :
-  QDialog(parent),
-  ui(new Ui::AutosaveSwcListDialog)
-{
+AutosaveSwcListDialog::AutosaveSwcListDialog(QWidget* parent)
+  : QDialog(parent)
+  , ui(new Ui::AutosaveSwcListDialog) {
   ui->setupUi(this);
   ui->listView->setModel(&m_fileList);
   ui->listView->setSelectionMode(QAbstractItemView::ExtendedSelection);
   connect(ui->listView, SIGNAL(doubleClicked(QModelIndex)),
-          this, SLOT(viewSwc(QModelIndex)));
+    this, SLOT(viewSwc(QModelIndex)));
   connect(ui->deleteSelectedPushButton, SIGNAL(clicked()),
-          this, SLOT(deleteSelected()));
+    this, SLOT(deleteSelected()));
 }
-
-AutosaveSwcListDialog::~AutosaveSwcListDialog()
-{
+AutosaveSwcListDialog::~AutosaveSwcListDialog() {
   delete ui;
 }
-
-void AutosaveSwcListDialog::updateFile()
-{
+void AutosaveSwcListDialog::updateFile() {
   m_fileList.loadDir(
-        NeutubeConfig::getInstance().getPath(NeutubeConfig::AUTO_SAVE).c_str());
+    NeutubeConfig::getInstance().getPath(NeutubeConfig::AUTO_SAVE).c_str());
 }
-
-QModelIndexList AutosaveSwcListDialog::getSelected() const
-{
-  QItemSelectionModel *model = ui->listView->selectionModel();
-
+QModelIndexList AutosaveSwcListDialog::getSelected() const {
+  QItemSelectionModel* model = ui->listView->selectionModel();
   return model->selectedIndexes();
 }
-
-void AutosaveSwcListDialog::deleteSelected()
-{
+void AutosaveSwcListDialog::deleteSelected() {
   QModelIndexList indexList = getSelected();
-  if (!indexList.isEmpty()) {
+  if(!indexList.isEmpty()) {
     QList<int> rowList;
-    foreach (const QModelIndex &index, indexList) {
+    foreach(const QModelIndex& index, indexList) {
       rowList.append(index.row());
     }
-    qSort(rowList);
-    for (int i = rowList.size() - 1; i >= 0; --i) {
+    std::sort(rowList.begin(), rowList.end());
+    for(int i = rowList.size() - 1; i >= 0; --i) {
       m_fileList.deleteFile(rowList[i]);
     }
   }
 }
-
-void AutosaveSwcListDialog::viewSwc(const QModelIndex &index)
-{
+void AutosaveSwcListDialog::viewSwc(const QModelIndex& index) {
   QString fileName = m_fileList.data(index).toString();
-
-  if (!fileName.isEmpty()) {
+  if(!fileName.isEmpty()) {
     fileName = "/" + fileName + ".swc";
     fileName =
-        NeutubeConfig::getInstance().getPath(NeutubeConfig::AUTO_SAVE).c_str() +
-        fileName;
-
-    ZStackDoc *doc = new ZStackDoc;
-    ZSwcTree *tree = new ZSwcTree;
+      NeutubeConfig::getInstance().getPath(NeutubeConfig::AUTO_SAVE).c_str() +
+      fileName;
+    ZStackDoc* doc = new ZStackDoc;
+    ZSwcTree* tree = new ZSwcTree;
     tree->load(fileName.toStdString());
-    if (!tree->isEmpty()) {
+    if(!tree->isEmpty()) {
       doc->addObject(tree);
       ZWindowFactory factory;
       factory.setParentWidget(this->parentWidget());
-      Z3DWindow *window = factory.make3DWindow(doc);
+      Z3DWindow* window = factory.make3DWindow(doc);
       window->show();
       window->raise();
     } else {
@@ -79,9 +65,7 @@ void AutosaveSwcListDialog::viewSwc(const QModelIndex &index)
     }
   }
 }
-
-void AutosaveSwcListDialog::on_pushButton_clicked()
-{
+void AutosaveSwcListDialog::on_pushButton_clicked() {
   ui->listView->setUpdatesEnabled(false);
   m_fileList.deleteAll();
   ui->listView->setUpdatesEnabled(true);
--- neurolabi/gui/dialogs/autosaveswclistdialog.h
+++ neurolabi/gui/dialogs/autosaveswclistdialog.h
@@ -1,27 +1,19 @@
 #ifndef AUTOSAVESWCLISTDIALOG_H
 #define AUTOSAVESWCLISTDIALOG_H
-
 #include <QDialog>
 #include "zswcfilelistmodel.h"
-
 namespace Ui {
 class AutosaveSwcListDialog;
 }
-
-class AutosaveSwcListDialog : public QDialog
-{
+class AutosaveSwcListDialog : public QDialog {
   Q_OBJECT
-
 public:
-  explicit AutosaveSwcListDialog(QWidget *parent = 0);
+  explicit AutosaveSwcListDialog(QWidget* parent = 0);
   ~AutosaveSwcListDialog();
-
   void updateFile();
-
 public slots:
   void deleteSelected();
-  void viewSwc(const QModelIndex &index);
-
+  void viewSwc(const QModelIndex& index);
 private slots:
   void on_pushButton_clicked();
 
@@ -29,8 +21,7 @@ private:
   QModelIndexList getSelected() const;
 
 private:
-  Ui::AutosaveSwcListDialog *ui;
+  Ui::AutosaveSwcListDialog* ui;
   ZSwcFileListModel m_fileList;
 };
-
 #endif // AUTOSAVESWCLISTDIALOG_H
